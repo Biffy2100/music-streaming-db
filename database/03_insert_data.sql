f9ed7196d3a80efd406901738b8880b8fa2bc6f1
vishal560 <vishal@560mtek.com>
2026-08-22 15:26:33 +0530

    Finished upto 2nd review
    2ND review included 10 DML queries in a word document (in english) that would be used for our project

--- database/03_insert_data.sql
+++ database/03_insert_data.sql
@@ -15,6 +15,8 @@ DELETE FROM playlist_song;
 
 DELETE FROM episode;
 
+DELETE FROM podcast;
+
 DELETE FROM song;
 
 DELETE FROM playlist;
@@ -28,8 +30,6 @@ DELETE FROM artist;
 DELETE FROM subscription;
 
 DELETE FROM users;
-
-ALTER TABLE user_episode AUTO_INCREMENT = 1;
 
 ALTER TABLE listening_history AUTO_INCREMENT = 1;
 
@@ -107,13 +107,19 @@ VALUES (
 INSERT INTO
     artist (Artist_Name, Country)
 VALUES ('Arijit Singh', 'India'),
+    ('A.R. Rahman', 'India'),
+    ('Shreya Ghoshal', 'India'),
     ('The Weeknd', 'Canada'),
     ('Coldplay', 'United Kingdom'),
     (
         'Taylor Swift',
         'United States'
     ),
-    ('Tame Impala', 'Australia');
+    ('Amit Trivedi', 'India'),
+    ('Armaan Malik', 'India'),
+    ('Tame Impala', 'Australia'),
+    ('jschlatt', 'United States'),
+    ('Radiohead', 'United Kingdom');
 
 INSERT INTO
     genre (Genre_Name)
@@ -121,7 +127,9 @@ VALUES ('Bollywood'),
     ('R&B'),
     ('Rock'),
     ('Pop'),
-    ('Indie');
+    ('Indie'),
+    ('Electronic'),
+    ('Christmas');
 
 INSERT INTO
     album (
@@ -129,15 +137,43 @@ INSERT INTO
         Release_Date,
         Artist_ID
     )
-VALUES ('Tum Hi Ho', '2013-04-26', 1),
+VALUES ('Aashiqui 2', '2013-04-26', 1),
+    ('Tamasha', '2015-11-06', 1),
+    ('Rockstar', '2011-11-15', 2),
+    ('Tamasha', '2015-11-06', 2),
+    ('Devdas', '2002-07-12', 3),
     (
         'After Hours',
         '2020-03-20',
-        2
+        4
     ),
-    ('Parachutes', '2000-07-10', 3),
-    ('1989', '2014-10-27', 4),
-    ('Currents', '2015-07-17', 5);
+    ('Parachutes', '2000-07-10', 5),
+    (
+        'A Rush of Blood to the Head',
+        '2002-08-26',
+        5
+    ),
+    ('1989', '2014-10-27', 6),
+    ('folklore', '2020-07-24', 6),
+    ('Dev.D', '2009-02-06', 7),
+    (
+        'Wake Up Sid',
+        '2009-10-02',
+        7
+    ),
+    ('Azhar', '2016-05-13', 8),
+    ('Hero', '2015-09-11', 8),
+    ('Currents', '2015-07-17', 9),
+    (
+        'A Very 1999 Christmas (Deluxe)',
+        '2025-12-04',
+        10
+    ),
+    (
+        'In Rainbows',
+        '2007-10-10',
+        11
+    );
 
 INSERT INTO
     song (
@@ -153,19 +189,128 @@ VALUES ('Tum Hi Ho', 262, 1, 1),
         1,
         1
     ),
-    ('Blinding Lights', 200, 2, 2),
-    ('Save Your Tears', 216, 2, 2),
-    ('Yellow', 266, 3, 3),
-    ('Shiver', 304, 3, 3),
-    ('Blank Space', 231, 4, 4),
-    ('Style', 231, 4, 4),
+    ('Hum Mar Jayenge', 245, 1, 1),
+    (
+        'Agar Tum Saath Ho',
+        341,
+        2,
+        1
+    ),
+    ('Kun Faya Kun', 470, 3, 1),
+    ('Nadaan Parindey', 396, 3, 1),
+    ('Matargashti', 302, 4, 1),
+    ('Dola Re Dola', 367, 5, 1),
+    (
+        'Silsila Ye Chahat Ka',
+        295,
+        5,
+        1
+    ),
+    ('Maar Dala', 284, 5, 1),
+    ('Blinding Lights', 200, 6, 2),
+    ('Save Your Tears', 216, 6, 2),
+    ('After Hours', 361, 6, 2),
+    ('Yellow', 266, 7, 3),
+    ('Shiver', 304, 7, 3),
+    ('Clocks', 307, 8, 3),
+    ('Blank Space', 231, 9, 4),
+    ('Style', 231, 9, 4),
+    ('Wildest Dreams', 220, 9, 4),
+    ('cardigan', 239, 10, 5),
+    (
+        'Emosanal Atyachar',
+        212,
+        11,
+        1
+    ),
+    ('Pardesi', 270, 11, 1),
+    ('Iktara', 288, 12, 1),
+    ('Bol Do Na Zara', 221, 13, 4),
+    (
+        'Main Hoon Hero Tera',
+        225,
+        14,
+        4
+    ),
     (
         'The Less I Know the Better',
         216,
-        5,
+        15,
         5
     ),
-    ('Eventually', 318, 5, 5);
+    ('Eventually', 318, 15, 5),
+    ('Let It Happen', 430, 15, 6),
+    (
+        'Let It Snow! Let It Snow! Let It Snow!',
+        119,
+        16,
+        7
+    ),
+    (
+        'Santa Claus Is Coming to Town',
+        137,
+        16,
+        7
+    ),
+    (
+        'Baby, It''s Cold Outside',
+        196,
+        16,
+        7
+    ),
+    ('White Christmas', 166, 16, 7),
+    (
+        'It''s the Most Wonderful Time of the Year',
+        132,
+        16,
+        7
+    ),
+    (
+        'The Christmas Song',
+        190,
+        16,
+        7
+    ),
+    (
+        'Have Yourself a Merry Little Christmas',
+        180,
+        16,
+        7
+    ),
+    ('Happy Holiday', 133, 16, 7),
+    (
+        '(Everybody''s Waitin'' for) The Man with the Bag',
+        137,
+        16,
+        7
+    ),
+    (
+        'Mele Kalikimaka (Merry Christmas)',
+        155,
+        16,
+        7
+    ),
+    ('Sleigh Ride', 159, 16, 7),
+    ('15 Step', 238, 17, 3),
+    ('Bodysnatchers', 242, 17, 3),
+    ('Nude', 255, 17, 3),
+    (
+        'Weird Fishes/Arpeggi',
+        318,
+        17,
+        3
+    ),
+    ('All I Need', 228, 17, 3),
+    ('Faust Arp', 129, 17, 3),
+    ('Reckoner', 290, 17, 3),
+    ('House of Cards', 328, 17, 3),
+    (
+        'Jigsaw Falling into Place',
+        248,
+        17,
+        3
+    ),
+    ('Videotape', 279, 17, 3);
 
 -- User-created collections and podcasts
 INSERT INTO
@@ -188,6 +333,66 @@ VALUES (
         'Weekend Mix',
         '2024-06-01',
         3
+    ),
+    (
+        'Bollywood Gold',
+        '2024-02-14',
+        1
+    ),
+    (
+        'Late Night R&B',
+        '2024-04-05',
+        2
+    ),
+    (
+        'Rock Classics',
+        '2024-06-15',
+        3
+    ),
+    (
+        'Pop Essentials',
+        '2024-07-20',
+        4
+    ),
+    (
+        'Indie Discoveries',
+        '2024-08-01',
+        1
+    ),
+    (
+        'Christmas Countdown',
+        '2024-11-01',
+        2
+    ),
+    (
+        'Rainy Day Radiohead',
+        '2024-08-12',
+        3
+    ),
+    (
+        'Workout Energy',
+        '2024-08-18',
+        4
+    ),
+    (
+        'Study Session',
+        '2024-09-02',
+        1
+    ),
+    (
+        'Road Trip Mix',
+        '2024-09-10',
+        2
+    ),
+    (
+        'Acoustic Evenings',
+        '2024-09-18',
+        3
+    ),
+    (
+        'All-Time Favorites',
+        '2024-10-01',
+        4
     );
 
 INSERT INTO
@@ -199,6 +404,10 @@ VALUES (
     (
         'History Unpacked',
         'Accessible stories from around the world.'
+    ),
+    (
+        'Chuckle Sandwich',
+        'A comedy podcast featuring conversations, stories, and absurd debates.'
     );
 
 INSERT INTO
@@ -225,60 +434,149 @@ VALUES (
         2360,
         '2024-08-05',
         2
+    ),
+    (
+        'The Internet Is Weird',
+        1980,
+        '2024-08-12',
+        3
+    ),
+    (
+        'The Sandwich Debate',
+        2240,
+        '2024-08-19',
+        3
+    ),
+    (
+        'A Very Serious Conversation',
+        1760,
+        '2024-08-26',
+        3
     );
 
 -- Many-to-many relationships
 INSERT INTO
     playlist_song (Playlist_ID, Song_ID)
 VALUES (1, 1),
-    (1, 3),
-    (1, 5),
-    (1, 7),
-    (2, 3),
+    (1, 11),
+    (1, 14),
+    (1, 17),
+    (1, 26),
     (2, 5),
-    (2, 9),
-    (3, 2),
+    (2, 15),
+    (2, 20),
+    (2, 42),
+    (2, 45),
     (3, 4),
-    (3, 6),
-    (3, 8),
-    (3, 10);
+    (3, 12),
+    (3, 19),
+    (3, 28),
+    (3, 39),
+    (3, 49);
 
 INSERT INTO
     artist_song (Artist_ID, Song_ID)
 VALUES (1, 1),
     (1, 2),
-    (2, 3),
-    (2, 4),
-    (3, 5),
-    (3, 6),
-    (4, 7),
-    (4, 8),
-    (5, 9),
-    (5, 10);
+    (1, 3),
+    (1, 4),
+    (2, 5),
+    (2, 6),
+    (2, 7),
+    (3, 8),
+    (3, 9),
+    (3, 10),
+    (4, 11),
+    (4, 12),
+    (4, 13),
+    (5, 14),
+    (5, 15),
+    (5, 16),
+    (6, 17),
+    (6, 18),
+    (6, 19),
+    (6, 20),
+    (7, 21),
+    (7, 22),
+    (7, 23),
+    (8, 24),
+    (8, 25),
+    (9, 26),
+    (9, 27),
+    (9, 28),
+    (10, 29),
+    (10, 30),
+    (10, 31),
+    (10, 32),
+    (10, 33),
+    (10, 34),
+    (10, 35),
+    (10, 36),
+    (10, 37),
+    (10, 38),
+    (10, 39),
+    (11, 40),
+    (11, 41),
+    (11, 42),
+    (11, 43),
+    (11, 44),
+    (11, 45),
+    (11, 46),
+    (11, 47),
+    (11, 48),
+    (11, 49);
 
 INSERT INTO
     user_artist (User_ID, Artist_ID)
 VALUES (1, 1),
     (1, 2),
-    (1, 3),
+    (1, 4),
+    (1, 10),
+    (1, 11),
     (2, 3),
     (2, 4),
+    (2, 5),
+    (2, 6),
+    (2, 10),
     (3, 2),
     (3, 5),
+    (3, 9),
+    (3, 11),
     (4, 1),
-    (4, 4);
+    (4, 4),
+    (4, 6),
+    (4, 8),
+    (4, 10);
 
 -- Playback history
 INSERT INTO
     listening_history (User_ID, Song_ID, Played_At)
 VALUES (1, 1, '2024-08-10 08:15:00'),
-    (1, 3, '2024-08-10 08:20:00'),
-    (1, 5, '2024-08-10 08:24:00'),
+    (1, 11, '2024-08-10 08:20:00'),
+    (1, 14, '2024-08-10 08:24:00'),
+    (1, 40, '2024-08-10 08:30:00'),
+    (1, 29, '2024-08-11 22:15:00'),
+    (1, 42, '2024-08-12 22:20:00'),
+    (1, 1, '2024-08-13 08:10:00'),
+    (1, 11, '2024-08-14 08:12:00'),
     (2, 3, '2024-08-11 09:00:00'),
     (2, 7, '2024-08-11 09:05:00'),
+    (2, 15, '2024-08-12 09:10:00'),
+    (2, 20, '2024-08-13 09:15:00'),
+    (2, 30, '2024-08-14 18:00:00'),
+    (2, 41, '2024-08-15 18:05:00'),
     (3, 9, '2024-08-12 18:30:00'),
+    (3, 16, '2024-08-12 18:36:00'),
+    (3, 26, '2024-08-13 19:00:00'),
+    (3, 43, '2024-08-14 19:05:00'),
+    (3, 44, '2024-08-15 19:10:00'),
+    (3, 28, '2024-08-16 19:20:00'),
     (4, 1, '2024-08-13 20:00:00'),
-    (4, 3, '2024-08-13 20:05:00');
+    (4, 3, '2024-08-13 20:05:00'),
+    (4, 18, '2024-08-14 20:10:00'),
+    (4, 24, '2024-08-15 20:15:00'),
+    (4, 31, '2024-08-16 20:20:00'),
+    (4, 49, '2024-08-17 20:25:00');
 
 INSERT INTO
     user_episode (
@@ -288,6 +586,12 @@ INSERT INTO
     )
 VALUES (1, 1, '2024-08-10 10:00:00'),
     (1, 2, '2024-08-11 10:00:00'),
+    (1, 4, '2024-08-12 10:00:00'),
+    (1, 5, '2024-08-13 10:00:00'),
     (2, 3, '2024-08-12 07:30:00'),
+    (2, 4, '2024-08-13 07:35:00'),
+    (2, 6, '2024-08-14 07:40:00'),
     (3, 1, '2024-08-13 12:15:00'),
-    (4, 2, '2024-08-14 16:45:00');
+    (3, 3, '2024-08-14 12:20:00'),
+    (4, 2, '2024-08-14 16:45:00'),
+    (4, 5, '2024-08-15 16:50:00');
--- database/04_queries.sql
+++ database/04_queries.sql
@@ -1,1 +1,116 @@
 USE music_streaming_db;
+
+SELECT * FROM ARTIST;
+
+SELECT * FROM GENRE;
+
+SELECT * FROM ALBUM;
+
+SELECT * FROM SONG;
+
+SELECT Song_ID, Title, Album_ID FROM SONG ORDER BY Song_ID;
+
+SELECT COUNT(*) AS total_songs FROM song;
+
+SELECT a.Artist_Name, COUNT(*) AS song_count
+FROM
+    artist a
+    JOIN artist_song artist_map ON a.Artist_ID = artist_map.Artist_ID
+GROUP BY
+    a.Artist_ID,
+    a.Artist_Name
+ORDER BY a.Artist_ID;
+
+-- =========================================
+-- DATABASE VERIFICATION
+-- =========================================
+
+-- Expected catalog and relationship counts
+SELECT 'users' AS table_name, COUNT(*) AS row_count
+FROM users
+UNION ALL
+SELECT 'subscription', COUNT(*)
+FROM subscription
+UNION ALL
+SELECT 'artist', COUNT(*)
+FROM artist
+UNION ALL
+SELECT 'genre', COUNT(*)
+FROM genre
+UNION ALL
+SELECT 'album', COUNT(*)
+FROM album
+UNION ALL
+SELECT 'song', COUNT(*)
+FROM song
+UNION ALL
+SELECT 'playlist', COUNT(*)
+FROM playlist
+UNION ALL
+SELECT 'playlist_song', COUNT(*)
+FROM playlist_song
+UNION ALL
+SELECT 'artist_song', COUNT(*)
+FROM artist_song
+UNION ALL
+SELECT 'user_artist', COUNT(*)
+FROM user_artist
+UNION ALL
+SELECT 'listening_history', COUNT(*)
+FROM listening_history
+UNION ALL
+SELECT 'podcast', COUNT(*)
+FROM podcast
+UNION ALL
+SELECT 'episode', COUNT(*)
+FROM episode
+UNION ALL
+SELECT 'user_episode', COUNT(*)
+FROM user_episode;
+
+-- Every song must have an artist, album, and genre
+SELECT s.Song_ID, s.Title
+FROM
+    song s
+    LEFT JOIN album alb ON s.Album_ID = alb.Album_ID
+    LEFT JOIN genre g ON s.Genre_ID = g.Genre_ID
+    LEFT JOIN artist_song artist_map ON s.Song_ID = artist_map.Song_ID
+WHERE
+    alb.Album_ID IS NULL
+    OR g.Genre_ID IS NULL
+    OR artist_map.Song_ID IS NULL;
+
+-- Album artists and song artists must agree
+SELECT
+    s.Song_ID,
+    s.Title,
+    alb.Album_Name,
+    alb.Artist_ID AS album_artist_id,
+    artist_map.Artist_ID AS song_artist_id
+FROM
+    song s
+    JOIN album alb ON s.Album_ID = alb.Album_ID
+    JOIN artist_song artist_map ON s.Song_ID = artist_map.Song_ID
+WHERE
+    alb.Artist_ID <> artist_map.Artist_ID;
+
+-- Confirm the two requested catalog sections
+SELECT a.Artist_Name, alb.Album_Name, COUNT(*) AS song_count
+FROM artist a
+    JOIN album alb ON a.Artist_ID = alb.Artist_ID
+    JOIN song s ON alb.Album_ID = s.Album_ID
+WHERE
+    a.Artist_Name IN ('jschlatt', 'Radiohead')
+GROUP BY
+    a.Artist_Name,
+    alb.Album_Name;
+
+-- Confirm Chuckle Sandwich is populated
+SELECT p.Podcast_Name, COUNT(e.Episode_ID) AS episode_count
+FROM podcast p
+    LEFT JOIN episode e ON p.Podcast_ID = e.Podcast_ID
+WHERE
+    p.Podcast_Name = 'Chuckle Sandwich'
+GROUP BY
+    p.Podcast_ID,
+    p.Podcast_Name;
