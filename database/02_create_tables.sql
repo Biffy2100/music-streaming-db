6695413ca0463194632963aa6654feff5bf0f530
vishal560 <vishal@560mtek.com>
2026-08-22 08:26:40 +0530

    created a few tables
    didn't insert data yet

--- database/02_create_tables.sql
+++ database/02_create_tables.sql
@@ -1,7 +1,6 @@
 USE music_streaming_db;
 
-DROP TABLE IF EXISTS subscription;
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS subscription, users, artist, album, genre; 
 
 CREATE TABLE USERS(
     User_ID INT PRIMARY KEY AUTO_INCREMENT,
@@ -18,4 +17,21 @@ CREATE TABLE subscription(
     User_ID INT NOT NULL UNIQUE,
 
     FOREIGN KEY (User_ID) REFERENCES USERS(User_ID)
+);
+
+CREATE TABLE artist(
+    Artist_ID INT PRIMARY KEY AUTO_INCREMENT,
+    Artist_Name VARCHAR(100) NOT NULL,
+    Country VARCHAR(100)
+);
+
+CREATE TABLE genre(
+    Genre_ID INT PRIMARY KEY AUTO_INCREMENT,
+    Genre_Name VARCHAR(50) NOT NULL UNIQUE
+);
+
+create table album(
+    Album_id int primary key AUTO_INCREMENT,
+    Album_Name varchar(100) not NULL,
+    Release_date DATE
 );
